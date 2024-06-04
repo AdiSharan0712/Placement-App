a4fc0a14fe5cbb9f881142c431bbee5f2f8b98dd
AdiSharan0712 <139259780+AdiSharan0712@users.noreply.github.com>
2024-06-04 17:39:56 +0530

    Create JobScreen.dart

---
+++ companies/JobScreen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class JobListScreen extends StatelessWidget {
+  final Company company;
+
+  JobListScreen({required this.company});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${company.name} - Job Openings'),
+      ),
+      body: ListView.builder(
+        itemCount: company.jobOpenings.length,
+        itemBuilder: (context, index) {
+          final job = company.jobOpenings[index];
+          return ListTile(
+            title: Text(job.title),
+            subtitle: Text('${job.description}\nCTC: ${job.ctc}'),
+          );
+        },
+      ),
+    );
+  }
+}
