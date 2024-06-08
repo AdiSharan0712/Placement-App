0065c5879dc2375a191b89b5263cd41eba406d87
AdiSharan0712 <139259780+AdiSharan0712@users.noreply.github.com>
2024-06-08 17:50:02 +0530

    Create JobOpeningClassModel.dart

---
+++ Admin Features/Job openings/JobOpeningClassModel.dart
@@ -0,0 +1,16 @@
+Job opening class model: 
+class JobOpening {
+  final String id;
+  final String companyId;
+  final String title;
+  final String ctc;
+  final String description;
+
+  JobOpening({
+    required this.id,
+    required this.companyId,
+    required this.title,
+    required this.ctc,
+    required this.description,
+  });
+}
