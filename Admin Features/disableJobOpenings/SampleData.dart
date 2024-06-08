4fc52b5568a800695fce42159682164a1acb289e
AdiSharan0712 <139259780+AdiSharan0712@users.noreply.github.com>
2024-06-08 18:00:43 +0530

    Create SampleData.dart

---
+++ Admin Features/disableJobOpenings/SampleData.dart
@@ -0,0 +1,11 @@
+List<JobOpening> jobOpenings = [
+  JobOpening(
+    id: '1',
+    companyId: '1',
+    title: 'Software Engineer',
+    ctc: '6 LPA',
+    description: 'Develop and maintain software applications.',
+    disabled: false,
+  ),
+  // Add more job openings
+];
