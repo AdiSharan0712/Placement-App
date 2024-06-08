5b5bfcd45e40d952cddc81c1b59809f70318b4a7
AdiSharan0712 <139259780+AdiSharan0712@users.noreply.github.com>
2024-06-08 17:36:21 +0530

    Create ModelClasses.dart

---
+++ Admin Features/StudentAndCompanyManagement/ModelClasses.dart
@@ -0,0 +1,13 @@
+class Company {
+  final String id;
+  final String name;
+  final String ctc;
+  final String jobRole;
+
+  Company({
+    required this.id,
+    required this.name,
+    required this.ctc,
+    required this.jobRole,
+  });
+}
